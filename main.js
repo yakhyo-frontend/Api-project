b1383d89ce5704509905d42dab2fe69869979095
Yaxa <you@example.com>
2026-05-01 20:13:34 +0500

    api-project has updated

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const elList = document.querySelector('.list');
 request.addEventListener('readystatechange', () => {
   if (request.readyState === 4) {
     const data = JSON.parse(request.responseText);
-    data.forEach(({ title, id, completed }) => {
+    data.forEach(({ title, id, completed}) => {
       elList.innerHTML += `
       <div class="list-item">
       <h1 class="title">title : ${title.slice(0, 20)}...</h1>
@@ -12,9 +12,31 @@ request.addEventListener('readystatechange', () => {
       <p>completed : ${completed}</p>
       </div>
       `
-    });
+    })
   }
 });
 
 request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
 request.send();
+
+
+// const request = new XMLHttpRequest();
+// const elList = document.querySelector('.list');
+
+// request.addEventListener('readystatechange', () => {
+//   if (request.readyState === 4) {
+//     const data = JSON.parse(request.responseText);
+//     data.forEach(({ title, id, completed }) => {
+//       elList.innerHTML += `
+//       <div class="list-item">
+//       <h1 class="title">title : ${title.slice(0, 20)}...</h1>
+//       <p>id : ${id}</p>
+//       <p>completed : ${completed}</p>
+//       </div>
+//       `
+//     });
+//   }
+// });
+
+// request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+// request.send();
